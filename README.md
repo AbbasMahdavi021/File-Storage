f53984eacf8979fc01ed0402b61ff392e1fd3486
AbbasMahdavi021 <abbas.mahdavi021@gmail.com>
2024-04-27 16:21:19 -0700

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-File Storage NextJS
+# File Storage NextJS
+
+File Storage NextJS is a web application built with Next.js that allows users to store files securely, manage authentication, and share files with others. It is deployed on Vercel using Vercel Blob for file storage and Vercel KV for database operations.
+
+## Features
+
+- Authentication: Users can create accounts, log in, and manage their profiles securely.
+- File Storage: Store files reliably and securely using Vercel Blob.
+- File Sharing: Share files with others through secure links or user permissions.
+
+## Technologies Used
+
+- Next.js
+- Vercel Blob
+- Vercel KV
+- React
+- Tailwind CSS
+
+## Deployment
+
+The application is hosted and deployed on Vercel, ensuring reliability and scalability.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="bg-slate-300 flex min-h-screen flex-col items-center p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <a
           href="https://github.com/AbbasMahdavi021/file-storage"
           target="_blank"
         >
           <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-            File Storage
+            APP NAME
           </p>
         </a>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
@@ -23,7 +23,7 @@ export default function Home() {
             <Image
               src="/am-logo.png"
               alt="AM Logo"
-              className="dark:invert"
+              className="dark:invert w-auto"
               width={100}
               height={24}
               priority
